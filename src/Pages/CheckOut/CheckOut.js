4c78d9a49145f1a5d79cf00524b8d618195b041a
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-17 17:59:33 +0600

    checkout page

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
 
 const CheckOut = () => {
-
+    const [item, setItem] = useState([])
+    useEffect(() => {
+        fetch("../data.json")
+            .then(res => res.json())
+            .then(data => setItem(data))
+    }, [])
+    const { serviceId } = useParams()
+    const selectedService = item.filter(selectedItem => selectedItem.id === serviceId)
     const handleSubmit = e => {
         e.preventDefault()
         toast.success("Thanks for Booking us")
     }
     return (
         <div>
-            <div className='h-[90vh] flex justify-center items-center'>
+            <div className=' flex justify-center items-center'>
                 <div className='text-[#066163]'>
                     <h1 className='text-center text-4xl  font-mono'>Booking form</h1>
                     <div>
+
                         <form onSubmit={handleSubmit}>
+                            <div className='flex justify-between items-center border-2 border-emerald-600 p-4 m-4'>
+                                <div>
+                                    <div className='text-2xl'>
+                                        Your selected service
+                                        <p>Name: {selectedService[0]?.name}</p>
+                                        <p>Price: ${selectedService[0]?.price}</p>
+                                    </div>
+                                </div>
+                                <img className='w-[120px] h-[120px] rounded-full mt-6' src={selectedService[0]?.img} alt="" />
+                            </div>
+
                             <div>
                                 <label className='text-2xl p-2 block'>Full name</label>
                                 <input className='md:w-[520px] h-[40px] outline-none border-2 border-[#066163] rounded px-2 block' type="text" placeholder='Your full name' required />
