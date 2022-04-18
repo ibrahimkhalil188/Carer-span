c9477f953cc787fd7b60ece3971b7bb1f9f2a7c1
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-18 13:07:43 +0600

    reviews

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Reviews from '../Reviews/Reviews';
 import Footer from '../Shared/Footer/Footer';
 import Banner from './Banner/Banner';
 import Services from './Services/Services';
@@ -8,6 +9,7 @@ const Home = () => {
         <div className='bg-[#F2F2F2]'>
             <Banner></Banner>
             <Services></Services>
+            <Reviews></Reviews>
             <Footer />
         </div>
     );
