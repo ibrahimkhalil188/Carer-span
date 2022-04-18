ea1129c7e1d681272d3c36321875ad3cade60ae4
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-18 20:00:31 +0600

    responsive

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,15 +2,15 @@ import React from 'react';
 import profile from '../../Asset/profile.jpg'
 const About = () => {
     return (
-        <div className='h-[71vh] flex justify-around items-center'>
-            <div className='w-1/2'>
+        <div className='md:flex justify-around items-center'>
+            <div className='md:w-1/2 w-full p-4 md:p-0'>
                 <h1 className='text-4xl font-mono font-bold py-4 text-[#066163]'>Ibrahim khalil</h1>
-                <h3 className='text-xl font-mono font-semibold'>
+                <h3 className='text-xl font-mono font-semibold p-2'>
                     A Curious learner who learning web development for the last four months! In 2022 I have a few goals to be a junior MERN stack developer and join an internship program and then join a company as a junior MERN stack developer.
                     In the long run, I want to be a Full-stack developer. so that, I'm learning continuously.</h3>
             </div>
             <div>
-                <img className='w-[350px] h-[350px] rounded-full' src={profile} alt="" />
+                <img className='md:w-[350px] w-[250px] md:h-[350px] h-[250px] rounded-full' src={profile} alt="" />
             </div>
         </div>
     );
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div className='grid grid-cols-2 gap-10 container mx-auto pt-12'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-10 container mx-auto pt-12'>
             <div className='shadow-xl p-4 border-2'>
-                <h1 className='text-4xl font-mono p-4'>Why i'm using firebase?</h1>
+                <h1 className='text-2xl md:text-4xl font-mono p-4'>Why i'm using firebase?</h1>
                 <div className='text-lg font-mono'>
                     Usually firebase provide 6-7 services but now i'm using 2 services of firebase.
                     <ul>
@@ -14,7 +14,7 @@ const Blogs = () => {
                 </div>
             </div>
             <div className='shadow-xl p-4 border-2'>
-                <h1 className='text-4xl font-mono p-4'>What other options do you have to implement authentication?</h1>
+                <h1 className='text-2xl md:text-4xl font-mono p-4'>What other options do you have to implement authentication?</h1>
                 <div className='text-lg font-mono'>
                     <p>I have so many alternative option for implement authentication. Here is a short list</p>
                     <ul>
@@ -27,7 +27,7 @@ const Blogs = () => {
                 </div>
             </div>
             <div className='shadow-xl p-4 border-2'>
-                <h1 className='text-4xl font-mono p-4'>What other services does firebase provide other than authentication</h1>
+                <h1 className='text-2xl md:text-4xl font-mono p-4'>What other services does firebase provide other than authentication</h1>
                 <div className='text-lg font-mono'>
                     <p>Firebase provide 4-5 other services other than authentication. like...</p>
                     <ul>
@@ -42,7 +42,7 @@ const Blogs = () => {
 
             </div>
             <div className='shadow-xl p-4 border-2'>
-                <h1 className='text-4xl font-mono p-4'> Difference between authorization and authentication</h1>
+                <h1 className='text-2xl md:text-4xl font-mono p-4'> Difference between authorization and authentication</h1>
                 <div className='text-lg font-mono'>
                     <p>Authorization is a process where check who are you?</p>
                     <p>Authorization need your identity token</p>
